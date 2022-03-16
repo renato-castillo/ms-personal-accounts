1c92018b707eaca28a93a1880813a7912074a715
renato-castillo <renatocastillopingo@gmail.com>
2022-03-16 13:30:43 -0500

    Added eureka client

--- src/main/java/com/bootcamp/personalaccounts/MsPersonalAccountsApplication.java
+++ src/main/java/com/bootcamp/personalaccounts/MsPersonalAccountsApplication.java
@@ -2,8 +2,10 @@ package com.bootcamp.personalaccounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsPersonalAccountsApplication {
 
     public static void main(String[] args) {
